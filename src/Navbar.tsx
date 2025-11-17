2d1b9e2a477123ddaa863b7b813fd8aa1d270bd5
royaleventsworldwide <royaleventsworldwide@gmail.com>
2025-11-17 00:14:30 -0500

    Create Navbar.tsx

---
+++ src/Navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav style={{
+      backgroundColor: "black",
+      padding: "15px",
+      color: "gold",
+      fontSize: "20px",
+      fontWeight: "bold"
+    }}>
+      Royal Events Worldwide
+    </nav>
+  );
+}
