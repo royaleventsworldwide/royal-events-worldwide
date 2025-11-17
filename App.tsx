94d1ebaf528cb56346f11a219bb08f809af623fc
royaleventsworldwide <royaleventsworldwide@gmail.com>
2025-11-17 00:08:57 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,18 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="/edutainment" element={<h1>Edutainment Page</h1>} />
-        <Route path="/wellness" element={<h1>Wellness Page</h1>} />
-        <Route path="/bookings" element={<h1>Bookings Page</h1>} />
-        <Route path="/contact" element={<h1>Contact Page</h1>} />
-      </Routes>
-    </BrowserRouter>
+      <h1>Royal Events Worldwide</h1>
+      <p>Site is running correctly now.</p>
+    </>
   );
 }
 
